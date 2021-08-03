8d67835a6b05c1dab5872dff9e0f2461e50f1c05
hans <hans.krebs@me.com>
2021-08-03 07:22:51 +0200

    Refactor setViewportHeightresponsively into separate utils function

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Calculator from './Calculator';
+import { setViewportHeightresponsively } from './utils/setViewportHeightresponsively';
 
 function App() {
-  setViewportSize();
+  setViewportHeightresponsively();
 
   return (
     <div className="App">
@@ -24,7 +25,7 @@ function App() {
             rel="noopener noreferrer"
           >
             here
-        </a>
+          </a>
         </p>
       </header>
     </div>
@@ -32,24 +33,3 @@ function App() {
 }
 
 export default App;
-
-function setViewportSize() {
-  // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-  let vh = window.innerHeight * 0.01;
-  // Then we set the value in the --vh custom property to the root of the document
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-  // We listen to the resize event
-  window.addEventListener('resize', () => {
-    // We execute the same script as before
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-  });
-
-  /* usage in CSS:
-  max-height: calc(var(--vh, 1vh) * 60);
-  height: calc(var(--vh, 1vh) * 60);
-  
-  For best outcome, must use with:
-  box-sizing: border-box;
-  */
-}
